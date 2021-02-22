414dafca8342de37c8e5580ac6fcbd11e8f691df
Aditya Dhananjay <aditya.dhananjay@gmail.com>
2021-02-22 10:56:05 -0500

    Change to autoshart.sh to make sure that the configured IP address takes hold.

--- sdcard/autostart.sh
+++ sdcard/autostart.sh
@@ -2,7 +2,9 @@
 ifconfig -a | grep eth0
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
-	ifconfig eth0 10.113.5.6 netmask 255.255.0.0
+    ifconfig eth0 down
+    ifconfig eth0 192.168.1.43 up
+	ifconfig eth0 192.168.1.43 netmask 255.255.255.0
 	piradio&
 	rftool
 fi
