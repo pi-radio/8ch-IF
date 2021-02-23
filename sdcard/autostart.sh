050b29bbfab70637afb9db47fad7c81cf553eb11
Aditya Dhananjay <aditya.dhananjay@gmail.com>
2021-02-23 13:28:43 -0500

    Update autostart.sh

--- sdcard/autostart.sh
+++ sdcard/autostart.sh
@@ -6,7 +6,7 @@ if [ $RESULT -eq 0 ]; then
 	ifconfig eth0 10.1.1.43 up
 	ifconfig eth0 down
 	ifconfig eth0 10.1.1.43 up
-	ifconfig eth0 192.168.1.43 netmask 255.255.255.0
+	ifconfig eth0 10.1.1.43 netmask 255.255.255.0
 	piradio&
 	rftool
 fi
