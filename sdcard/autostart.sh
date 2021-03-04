3a3c7391df157fe0d89aaad58ce79b8e3cac4a1b
Aditya Dhananjay <aditya.dhananjay@gmail.com>
2021-03-04 13:10:37 -0500

    Fixed the MAC address issue

--- sdcard/autostart.sh
+++ sdcard/autostart.sh
@@ -7,6 +7,12 @@ if [ $RESULT -eq 0 ]; then
 	ifconfig eth0 down
 	ifconfig eth0 10.1.1.43 up
 	ifconfig eth0 10.1.1.43 netmask 255.255.255.0
+	/etc/init.d/networking stop
+	ifconfig eth0 hw ether 02:02:02:02:02:43
+	ifconfig eth0 10.1.1.43 up
+	/etc/init.d/networking start
+	sleep 3
+
 	piradio&
 	rftool
 fi
